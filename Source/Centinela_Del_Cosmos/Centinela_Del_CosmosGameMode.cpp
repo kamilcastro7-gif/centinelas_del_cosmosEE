af440808e1255139230456c847f8870e96511062
cristianbravocm4770845 <cristianbravocm4770845@gmail.com>
2026-05-10 17:14:47 -0400

    factory method agregado

--- Source/Centinela_Del_Cosmos/Centinela_Del_CosmosGameMode.cpp
+++ Source/Centinela_Del_Cosmos/Centinela_Del_CosmosGameMode.cpp
@@ -1,19 +1,14 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "Centinela_Del_CosmosGameMode.h"
-#include "EVastago_Del_infierno.h"
-#include "EVigia_Del_Vacio.h"
-#include "EHeraldo_De_La_Ruina.h"
+#include "EnemyFactory.h"
 #include "Facade.h"
 #include "Engine/World.h"
 
 ACentinela_Del_CosmosGameMode::ACentinela_Del_CosmosGameMode()
 {
     PrimaryActorTick.bCanEverTick = true;
-
-    ClaseVastago = AEVastago_Del_infierno::StaticClass();
-    ClaseVigia = AEVigia_Del_Vacio::StaticClass();
-    ClaseHeraldo = AEHeraldo_De_La_Ruina::StaticClass();
+    Factory = nullptr;
+    ManejadorHorda = nullptr;
 }
 
 void ACentinela_Del_CosmosGameMode::BeginPlay()
@@ -21,26 +16,19 @@ void ACentinela_Del_CosmosGameMode::BeginPlay()
     Super::BeginPlay();
 
     UWorld* const Mundo = GetWorld();
-    if (Mundo)
-    {
-        FActorSpawnParameters ParametrosSpawn;
-        ParametrosSpawn.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+    if (!Mundo) return;
 
-        // Origen central para el combate
-        FVector Origen(0.0f, 0.0f, 150.0f);
+    Factory = Mundo->SpawnActor<AEnemyFactory>(AEnemyFactory::StaticClass());
+    ManejadorHorda = Mundo->SpawnActor<AFacade>(AFacade::StaticClass());
 
-        // Spawn Vástago (Esfera)
-        Mundo->SpawnActor<AEVastago_Del_infierno>(ClaseVastago, Origen + FVector(200, 0, 0), FRotator::ZeroRotator, ParametrosSpawn);
+    if (!Factory) return;
 
-        // Spawn Vigía (Toroide - Ahora se moverá en Y)
-        Mundo->SpawnActor<AEVigia_Del_Vacio>(ClaseVigia, Origen + FVector(-200, 0, 0), FRotator::ZeroRotator, ParametrosSpawn);
+    const FVector   Origen = FVector(0.0f, 0.0f, 150.0f);
+    const FRotator  SinRotacion = FRotator::ZeroRotator;
 
-        // Spawn Heraldo (Cubo)
-        Mundo->SpawnActor<AEHeraldo_De_La_Ruina>(ClaseHeraldo, Origen + FVector(0, 300, 0), FRotator::ZeroRotator, ParametrosSpawn);
-
-        // Spawneamos la Fachada
-        ManejadorHorda = Mundo->SpawnActor<AFacade>(AFacade::StaticClass());
-    }
+    Factory->SpawnEnemigo(ETipoEnemigo::Vastago, Origen + FVector(200, 0, 0), SinRotacion);
+    Factory->SpawnEnemigo(ETipoEnemigo::Vigia, Origen + FVector(-200, 0, 0), SinRotacion);
+    Factory->SpawnEnemigo(ETipoEnemigo::Heraldo, Origen + FVector(0, 300, 0), SinRotacion);
 }
 
 void ACentinela_Del_CosmosGameMode::Tick(float DeltaTime)
@@ -52,4 +40,3 @@ void ACentinela_Del_CosmosGameMode::Tick(float DeltaTime)
         ManejadorHorda->MoverHorda();
     }
 }
-
--- Source/Centinela_Del_Cosmos/Centinela_Del_CosmosGameMode.h
+++ Source/Centinela_Del_Cosmos/Centinela_Del_CosmosGameMode.h
@@ -1,32 +1,23 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
-#include "Facade.h"
 #include "Centinela_Del_CosmosGameMode.generated.h"
 
 UCLASS()
 class CENTINELA_DEL_COSMOS_API ACentinela_Del_CosmosGameMode : public AGameModeBase
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	ACentinela_Del_CosmosGameMode();
+    ACentinela_Del_CosmosGameMode();
+
 protected:
-	virtual void BeginPlay() override;
-	virtual void Tick(float DeltaTime) override;
+    virtual void BeginPlay() override;
+    virtual void Tick(float DeltaTime) override;
 
 private:
-	// Puntero para controlar la Fachada
-	AFacade* ManejadorHorda;
-
-	// Clases para el Spawn (se asignan en el constructor o BP)
-	TSubclassOf<class AEVastago_Del_infierno> ClaseVastago;
-	TSubclassOf<class AEVigia_Del_Vacio> ClaseVigia;
-	TSubclassOf<class AEHeraldo_De_La_Ruina> ClaseHeraldo;
+    class AEnemyFactory* Factory;
+    class AFacade* ManejadorHorda;
 };
-
-
-
