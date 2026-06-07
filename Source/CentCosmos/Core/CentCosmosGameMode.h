ff5994c8834e96bc0905b5fda4e63b427dce4b75
Cristian <cristianbravocm4770845@gmail.com>
2026-06-07 17:45:08 -0400

    ultimos cambios para el menu

--- Source/CentCosmos/Core/CentCosmosGameMode.h
+++ Source/CentCosmos/Core/CentCosmosGameMode.h
@@ -5,22 +5,23 @@
 #include "Patterns/Builder/Nivel1Builder.h"   
 #include "Facade.h"                               
 #include "CentCosmosGameMode.generated.h"
+
 UCLASS()
 class CENTCOSMOS_API ACentCosmosGameMode : public AGameModeBase
 {
     GENERATED_BODY()
 public:
     ACentCosmosGameMode();
-
 protected:
     virtual void BeginPlay() override;
     virtual void Tick(float DeltaTime) override;
+    virtual void PostLogin(APlayerController* NewPlayer) override;
 
 private:
-    // Builder
     UPROPERTY() ANivelDirector* Director;
     UPROPERTY() ANivel1Builder* BuilderFacil;
     UPROPERTY() AFacade* ManejadorHorda;
 
-
+    FTimerHandle TimerHandle_InputFix;
+    void RestaurarInputJugador();
 };
